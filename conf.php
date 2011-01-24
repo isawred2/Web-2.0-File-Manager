2d668600397d4385a729ed8059795e4915413d2a
Vitali <vitmalina@gmail.com>
2011-01-24 13:36:45 -0600

    config changes

--- conf.php
+++ conf.php
@@ -6,6 +6,7 @@
 *************************************************************/
 
 // -- basic settions
+$sys_home		= str_replace('conf.php', '', substr(__FILE__, strlen($_SERVER['DOCUMENT_ROOT'])));
 $def_archive 	= 'zip'; // can be: zip, tar, tar.bz, tar.gz
 $def_safeFiles  = ''; // if blank - all safe, otherwise safe file extanctions separated by comma for example 'jpg,gif,doc,xls'
 $def_defaultCSS = 'simple-blue.css';
@@ -21,6 +22,4 @@ $def_folders = Array();
 $def_folders['Home'] 		= '/tmp/fm';
 $def_folders['Documents'] 	= '/tmp/fm_docs';
 $def_folders['Temp']	 	= '/tmp/fm_temp';
-
-$sys_home		= str_replace('conf.php', '', substr(__FILE__, strlen($_SERVER['DOCUMENT_ROOT'])));
 ?>
